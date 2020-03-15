e275d816b7af1e7af5a1d6a832108b95819c8b05
Christopher Chaison <cchaison0@gmail.com>
2020-03-15 21:35:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,8 +2,7 @@ class Song
 
   attr_accessor :name, :artist
 
-  def initialize(name, artist)
-    @name = name
-    @artist = new Artist(artist)
+  def initialize
+    @artist = Artist.new
   end
 end
